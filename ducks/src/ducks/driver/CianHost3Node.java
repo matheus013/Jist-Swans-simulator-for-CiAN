af75316793c9cf63c244d00d348c5240a2ebe03e
Jordan Alliot <jordan.alliot@gmail.com>
2011-08-08 15:11:10 +0000

    Starting implementation of a CiAN simulation
    git-svn-id: http://north.dsg.cs.tcd.ie/repos/simulator-workflow@7 6aedb14f-efc7-4c1d-8d98-6d0cd5b8577c

---
+++ ducks/src/ducks/driver/CianHost3Node.java
@@ -0,0 +1,72 @@
+package ducks.driver;
+
+import java.util.HashMap;
+
+import jist.swans.Constants;
+import jist.swans.field.Field;
+import jist.swans.field.Placement;
+import jist.swans.misc.Mapper;
+import jist.swans.net.NetInterface;
+
+import org.apache.log4j.Logger;
+
+import ext.jist.swans.app.AppGreetingBase;
+import ext.jist.swans.app.AppWorkflowBase;
+import ext.jist.swans.app.AppWorkflowProvider;
+import ext.util.stats.DucksCompositionStats;
+
+public class CianHost3Node extends GenericNode {
+    private static Logger log = Logger.getLogger(CianHost3Node.class.getName());
+
+    public CianHost3Node() {
+        super();
+    }
+
+    protected void addApplication(Mapper protMap, Field field, Placement place) throws Exception {
+        this.app = null;
+        String appOpt = options.getStringProperty(SimParams.TRAFFIC_TYPE);
+
+        if (appOpt.equals(SimParams.TRAFFIC_TYPE_SERVICE)) {
+            HashMap<Character, Integer> repository = new HashMap<Character, Integer>();
+            String mode = options.getStringProperty(SimParams.COMPOSITION_TYPE); 
+            int reqSize = options.getIntProperty(SimParams.COMPOSITION_LENGTH);
+            String compRestrict = options.getStringProperty(SimParams.COMPOSITION_RESTRICTION);
+                       
+            // init provider-specific service repository
+            char service='A';
+            int execTime=0;
+            while (repository.size()<reqSize) {
+                execTime = options.getIntProperty(SimParams.SERVICE_EXEC_TIME_PREFIX+"."+service+"."+this.id);
+                repository.put(service, execTime);
+//                System.out.println("Add to rep: id="+this.id+" service="+serviceString+" exec="+execTime);
+                service++;
+            }
+                                                
+            DucksCompositionStats compoStats = null;
+            try {
+                // get composition stats, and automatically create and register, if necessary
+//                compoStats = (DucksCompositionStats) stats.getStaticCollector(DucksCompositionStats.class, true);
+                // TODO: Christin, decide whether provider nodes should print out compo stats, too
+                compoStats = new DucksCompositionStats();
+            } catch (Exception e) {
+                
+            }    
+            
+            AppWorkflowBase ac = new AppWorkflowProvider(this.id, compoStats, mode, repository, compRestrict);
+            
+            protMap.testMapToNext(Constants.NET_PROTOCOL_UDP);
+            net.setProtocolHandler(Constants.NET_PROTOCOL_UDP, (NetInterface.NetHandler) ac.getNetProxy());
+
+            protMap.testMapToNext(AppGreetingBase.NET_PROTOCOL_NUMBER);
+            net.setProtocolHandler(AppGreetingBase.NET_PROTOCOL_NUMBER, (NetInterface.NetHandler) ac.getNetProxy());
+
+            this.app = ac;
+            this.appEntity = ac.getAppProxy();
+            ac.setNetEntity(this.netEntity);
+            this.appEntity.run();
+
+            // stats.registerStaticCollector(as);
+            log.debug("  Added composition provider application module");
+        }
+    }
+}
---
+++ ducks/src/ducks/driver/CianPlannerNode.java
@@ -0,0 +1,61 @@
+package ducks.driver;
+
+import jist.swans.Constants;
+import jist.swans.field.Field;
+import jist.swans.field.Placement;
+import jist.swans.misc.Mapper;
+import jist.swans.net.NetInterface;
+
+import org.apache.log4j.Logger;
+
+import ext.jist.swans.app.AppGreetingBase;
+import ext.jist.swans.app.AppWorkflowBase;
+import ext.jist.swans.app.AppWorkflowInitiator;
+import ext.util.stats.DucksCompositionStats;
+
+public class CianPlannerNode extends GenericNode {
+    private static Logger log = Logger.getLogger(CianPlannerNode.class.getName());
+
+    public CianPlannerNode() {
+        super();
+    }
+
+    protected void addApplication(Mapper protMap, Field field, Placement place) throws Exception {
+        this.app = null;
+        String appOpt = options.getStringProperty(SimParams.TRAFFIC_TYPE);
+
+        if (appOpt.equals(SimParams.TRAFFIC_TYPE_SERVICE)) {
+            int reqSize, reqRate, wTS, wTE, duration;
+            reqSize = options.getIntProperty(SimParams.COMPOSITION_LENGTH);
+            reqRate = options.getIntProperty(SimParams.TRAFFIC_SERV_RATE);
+            wTS = options.getIntProperty(SimParams.TRAFFIC_WAITSTART);
+            wTE = options.getIntProperty(SimParams.TRAFFIC_WAITEND);
+            duration = globalConfig.getIntProperty(SimParams.SIM_DURATION);
+            
+            DucksCompositionStats compoStats = null;
+            try {
+                // get composition stats, and automatically create and register, if necessary
+                compoStats = (DucksCompositionStats) stats.getStaticCollector(DucksCompositionStats.class, true);
+            } catch (Exception e) {
+                e.printStackTrace();                
+            }  
+
+            AppWorkflowBase ac = new AppWorkflowInitiator(this.id, compoStats, reqSize, reqRate, wTS, wTE, duration);
+
+            // currently we do not use transport layer (UDP) instead we hand over from the network layer directly to application layer 
+            protMap.testMapToNext(Constants.NET_PROTOCOL_UDP);
+            net.setProtocolHandler(Constants.NET_PROTOCOL_UDP, (NetInterface.NetHandler) ac.getNetProxy());
+
+            protMap.testMapToNext(AppGreetingBase.NET_PROTOCOL_NUMBER);
+            net.setProtocolHandler(AppGreetingBase.NET_PROTOCOL_NUMBER, (NetInterface.NetHandler) ac.getNetProxy());
+
+            this.app = ac;
+            this.appEntity = ac.getAppProxy();
+            ac.setNetEntity(this.netEntity);
+            this.appEntity.run();
+
+            log.debug("  Added composition initiator application module");
+        }
+    }
+
+}
